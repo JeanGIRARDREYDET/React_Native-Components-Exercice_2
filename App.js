0d730107e8c71374bbe83d72f7dbe3968595784f
cogitafac <cogitafac@M1.local>
2022-05-11 11:15:02 +0200

    Photo et text

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./img/opportunity.jpg')} style={styles.container1}/>
+      <Text style={styles.container2}>Le rover Opportunity</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: '#fff',
+    alignItems: 'center', 
+    justifyContent: 'center',
+  }
+  ,container1: {
+    flex: 3,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+  ,container2: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
